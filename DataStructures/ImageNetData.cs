3f7c7231d69930e75765fb833295f92b93fb181b
Bryan Scheetz <bryan.scheetz@proconex.net>
2024-12-19 23:21:58 -0500

    Add project files.

---
+++ DataStructures/ImageNetData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.ML.Data;
+
+/// <summary>
+/// ImageNetData is the input image data class
+///     [ImagePath] is the path to the image
+///     [Label] is the name of the file
+///     ReadFromFile() is for collections of ImageNetData objects
+/// </summary>
+
+namespace DataStructures;
+
+public class ImageNetData
+{
+    [LoadColumn(0)]
+    public string ImagePath;
+
+    [LoadColumn(1)]
+    public string Label;
+
+    public static IEnumerable<ImageNetData> ReadFromFile(string imageFolder)
+    {
+        return Directory
+            .GetFiles(imageFolder)
+            .Where(filePath => Path.GetExtension(filePath) != ".md")
+            .Select(filePath => new ImageNetData { ImagePath = filePath, Label = Path.GetFileName(filePath) });
+    }
+}
+
+
---
+++ DataStructures/ImageNetPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace DataStructures;
+
+public class ImageNetPrediction
+{
+    /// <summary>
+    /// Prediction results class. Model returns a flat tensor as a float[]
+    ///     Contains the dimensions, objectness score, and class probabilities for each of the bounding boxes
+    /// </summary>
+    [ColumnName("grid")]
+    public float[] PredictedLabels;
+}
